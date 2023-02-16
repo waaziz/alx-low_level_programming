40354d66a066d2cc61a225ff8d92119ab499644a
reda <rwazizo@gmail.com>
2023-02-16 14:38:28 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - prints exactly and that piece of art
+ * is useful" - Dora Korpar, 2015-10-19, followed by a
+ * new line, to the standard error.
+ * Return: Always 1.
+ */
+
+
+int main(void) {
+    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, str, sizeof(str) - 1);
+    return 1;
+}
