34bad61213309545d7a823721e542e67b0cf1738
reda <rwazizo@gmail.com>
2023-02-16 11:55:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,12 @@
 /**
  * main - prints "Proramming is like building a multilingual
  * puzzle, followed by a newline.
- * return 0.
+ * Return: Always 0.
  */
 
 int main(void)
 	
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
 	return (0);
 }	
