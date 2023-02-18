6cf3670e56b5bd228e3cae11077ebc989e20da9d
reda <rwazizo@gmail.com>
2023-02-18 07:45:09 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,14 @@
  * Return: Always 0.
  */
 
-int main(void)	
+int main(void)
 {
 	char i;
 	
-	for(i = 'a' ; i <= 'z' ; i++)
+	for (i = 'a' ; i <= 'z' ; i++)
 	{
 		putchar(i);
 	}
 	putchar ('\n');
 	return (0);
-}	
+}
