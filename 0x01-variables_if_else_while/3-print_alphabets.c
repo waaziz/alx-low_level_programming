522d430168bb13278a25f40584ebbfb0c2fff786
reda <rwazizo@gmail.com>
2023-02-18 08:32:30 -0800

    3-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,15 @@
 
 int main(void)
 {
-        char i;
-
-        for (i = 'a' ; i <= 'z' ; i++)
-        {
-                putchar(i);
-        }
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
 	for (i = 'A' ; i <= 'Z'; i++)
 	{
 		putchar(i);
 	}
-        putchar ('\n');
-        return (0);
+	putchar ('\n');
+	return (0);
 }
