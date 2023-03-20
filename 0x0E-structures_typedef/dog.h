4fe099bc496bcce677118e3ff6f0aad8d7756d3a
reda <rwazizo@gmail.com>
2023-03-20 15:39:24 -0700

    qffez

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,11 @@
 #define DOG_H
 
 /**
-* struct dog - a dog's basic info
-* @name: First member
-* @age: Second member
-* @owner: Third member
-* Description: Longer description
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: Longer description
 */
 
 struct dog
